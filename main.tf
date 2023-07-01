ab1f40598b7adc71c83d4f1dafc807a3c36cca0f
Sagar Maurya <15274533+sagarising@users.noreply.github.com>
2023-07-01 18:49:09 +0530

    allow http access

--- main.tf
+++ main.tf
@@ -23,6 +23,15 @@ resource "aws_security_group" "allow_ssh" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  ingress {
+    description      = "http"
+    from_port        = 8000
+    to_port          = 8000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 65535
