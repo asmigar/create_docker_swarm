41175553f9628b168c8e64f429e09a7334b9c53f
Sagar Maurya <15274533+sagarising@users.noreply.github.com>
2023-07-04 11:39:19 +0530

    enable public dns

--- out.tf
+++ out.tf
@@ -1,4 +1,4 @@
-output "instance_public_ip" {
-  value = aws_instance.web.public_ip
-  description = "Public ip of the instance to connect to"
+output ssh_command {
+  value = "ssh ec2-user@${aws_instance.web.public_dns}"
+  description = "ssh command for connect to the instance"
 }
--- vpc.tf
+++ vpc.tf
@@ -5,6 +5,7 @@ data "aws_availability_zones" "available" {
 resource "aws_vpc" "main" {
   cidr_block       = var.vpc_cidr_block
   instance_tenancy = "default"
+  enable_dns_hostnames = true
 
   tags = {
     Name = "main"
