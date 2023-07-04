9294a6a949e4722380323db032b5b1d8316f8b49
Sagar Maurya <15274533+sagarising@users.noreply.github.com>
2023-07-04 19:58:33 +0530

    update name of the instance

--- main.tf
+++ main.tf
@@ -51,12 +51,12 @@ resource "aws_key_pair" "webserver" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDMK0sHujc2rfg3y4blCK46ye7jMTmDa3jXqGVL68ts87zo95JZdQakLQlLiUkVvm7IYOePctjUZDRtaUgnWPWdC/QYQvLLiAkO/IzgcHIyvURNjpVd94I67afYygzIeagsKQJW+igVeztw95fkwW8gvb2DirM4UkOEN6FnGMjBDOFjTJSu1AKqgzr7cqV7JMdVqDLlODGS6Q++H/xv2WTFNXNBR9NFTtTrxJl5+iFKrtg8D/T+1/5LvUwclDd9un9Jb3SYCtxwRK00I3E1nNSviT1zxsUrtkSMt8B+qngwUGjT5whIdpV3109qFt9F4/lEbcrpTtuoiH/nCgQYNL7CZMKVn12r0jg/ADOHpqj56sxh81Vx/NWRi4t1H8d7OZ/aQ3pFMtW1fB+kcdvwT1fNJRAW1U1m6rR2yd6QjUR+5jiBLfvpH6PIwdn1OpVPGRtAUIQxkOeRkTeUsAbvBm2PvWSnoAtgLv/8GzlzY00uw0odLlj/RhJlIHYkyXqETTs= sagarmaurya@sagar.local"
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "manager" {
   ami           = "ami-022e1a32d3f742bd8"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "webserver"
+    Name = "manager"
   }
 
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
--- out.tf
+++ out.tf
@@ -1,6 +1,6 @@
 output ssh_command {
   value = <<-SSHCOMMAND
-  ssh ec2-user@${aws_instance.web.public_dns}
+  ssh ec2-user@${aws_instance.manager.public_dns}
   SSHCOMMAND
   description = "ssh command for connect to the instance"
 }
