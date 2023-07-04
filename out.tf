08c81d844a568c14f0dd4384eaa713303e867bcf
Sagar Maurya <15274533+sagarising@users.noreply.github.com>
2023-07-04 11:50:48 +0530

    enhance ssh command output

--- out.tf
+++ out.tf
@@ -1,4 +1,6 @@
 output ssh_command {
-  value = "ssh ec2-user@${aws_instance.web.public_dns}"
+  value = <<-SSHCOMMAND
+  ssh ec2-user@${aws_instance.web.public_dns}
+  SSHCOMMAND
   description = "ssh command for connect to the instance"
 }
