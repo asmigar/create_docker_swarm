96ed16ffe33720f7107b4d051173bc2d2d48d4cf
Sagar Maurya <15274533+sagarising@users.noreply.github.com>
2023-07-12 17:32:11 +0530

    remove user from ssh command output

--- out.tf
+++ out.tf
@@ -1,6 +1,6 @@
 output "ssh_command_manager_node" {
   value       = <<-SSHCOMMAND
-  ssh ec2-user@${aws_instance.manager.public_dns}
+  ssh ${aws_instance.manager.public_dns}
   SSHCOMMAND
   description = "ssh command for connecting to the manager node"
 }
@@ -8,7 +8,7 @@ output "ssh_command_manager_node" {
 output "ssh_command_worker_nodes" {
   value       = <<-SSHCOMMAND
   %{for dns in aws_instance.worker[*].public_dns}
-  ssh ec2-user@${dns}
+  ssh ${dns}
   %{endfor}
   SSHCOMMAND
   description = "ssh command for connect to the worker node"
