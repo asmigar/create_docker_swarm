731d1c11fe51db1339f2f6f99d4cbbabeb3111fc
Sagar Maurya <15274533+sagarising@users.noreply.github.com>
2023-08-11 15:01:11 +0530

    add ssh key and user to ssh command output

--- infra/out.tf
+++ infra/out.tf
@@ -1,12 +1,12 @@
 output "ssh_command_manager_node" {
-  value       = "ssh ${aws_instance.manager.public_dns}"
+  value       = "ssh -i ~/.ssh/${aws_key_pair.this.key_name}.pem ec2-user@${aws_instance.manager.public_dns}"
   description = "ssh command for connecting to the manager node"
 }
 
 output "ssh_command_worker_nodes" {
   value       = <<-SSHCOMMAND
   %{for dns in aws_instance.worker[*].public_dns}
-  ssh ${dns}
+  ssh -i ~/.ssh/${aws_key_pair.this.key_name}.pem ec2-user@${dns}
   %{endfor}
   SSHCOMMAND
   description = "ssh command for connect to the worker node"
