0877c292868e5afcdfdbf18e6a16d04c0a61161a
Sagar Maurya <15274533+sagarising@users.noreply.github.com>
2023-08-18 12:58:09 +0530

    add network rules for overlay network traffic

--- infra/main.tf
+++ infra/main.tf
@@ -43,10 +43,26 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks = [aws_subnet.public.cidr_block]
   }
 
+  ingress {
+    description = "overlay network node discovery"
+    from_port   = 7946
+    to_port     = 7946
+    protocol    = "tcp"
+    cidr_blocks = [aws_subnet.public.cidr_block]
+  }
+
+  ingress {
+    description = "overlay network traffic"
+    from_port   = 4789
+    to_port     = 4789
+    protocol    = "udp"
+    cidr_blocks = [aws_subnet.public.cidr_block]
+  }
+
   egress {
     from_port        = 0
-    to_port          = 65535
-    protocol         = "tcp"
+    to_port          = 0
+    protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
 
