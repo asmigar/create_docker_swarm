e8b76a24a90035a5790aba57730753bd5429d3f5
Sagar Maurya <15274533+sagarising@users.noreply.github.com>
2023-08-31 21:23:16 +0530

    fix aws_s3_bucket arguments depreciation

--- remote_state/main.tf
+++ remote_state/main.tf
@@ -17,20 +17,25 @@ resource "aws_s3_bucket" "terraform_state" {
   lifecycle {
     prevent_destroy = true
   }
+}
 
-  # Enable versioning so we can see the full revision history of our
-  # state files
-  versioning {
-    enabled = true
-  }
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
 
-  # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
     }
+  }
+}
+
+
+# Enable versioning so we can see the full revision history of our state files
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
   }
 }
 
